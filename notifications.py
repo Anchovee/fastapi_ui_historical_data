51b26bf842975dd12d7401afc99279334196391d
Anchovee <68454871+Anchovee@users.noreply.github.com>
2021-01-14 12:41:58 -1000

    Add files via upload

---
+++ notifications.py
@@ -0,0 +1,13 @@
+import smtplib, ssl, config
+
+context = ssl.create_default_context()
+def email(messages):
+    with smtplib.SMTP_SSL(config.EMAIL_HOST, config.EMAIL_PORT, context=context) as server:
+        server.login(config.EMAIL_ADDRESS, config.EMAIL_PASSWORD)
+        server.login(config.EMAIL_ADDRESS, config.EMAIL_PASSWORD)
+
+        email_message = "\n\n".join(messages)    
+        server.sendmail(config.EMAIL_ADDRESS, config.EMAIL_ADDRESS, email_message)
+        server.sendmail(config.EMAIL_ADDRESS, config.EMAIL_SMS, email_message)
+
+        server.quit()
